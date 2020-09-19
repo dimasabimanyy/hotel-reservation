d70a590ea85e8408a0df73d0371dd4d68a91b084
dimassabimanyy <dmsabmny@gmail.com>
2020-09-19 11:26:41 +0700

    create services and featured rooms

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { RoomProvider } from "./context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RoomProvider>
+      <Router>
+        <App />
+      </Router>
+    </RoomProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,59 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Banner } from "../components/Banner";
+import Hero from "../components/Hero";
+import defaultBcg from "../images/room-1.jpeg";
+import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg,
+    };
+  }
+
+  static contextType = RoomContext;
+
+  componentDidMount() {}
+
   render() {
-    return <div>Sup from single room</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    console.log(room);
+
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room could be found</h3>
+          <Link to="/rooms" className="btn-primary">
+            Back to rooms
+          </Link>
+        </div>
+      );
+    }
+
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${room.name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            Back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
   }
 }
