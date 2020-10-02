c57aae4b03da7be007efc87f0ebef7d854371807
dimassabimanyy <dmsabmny@gmail.com>
2020-10-02 20:19:03 +0700

    create - featured special items - section

--- src/components/FeaturedSpecial.js
+++ src/components/FeaturedSpecial.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Title from "./Title";
 import titleImg from "../images/section-title.png";
+import FeaturedSpecialItem from "./FeaturedSpecialItem";
+import img1 from "../images/special-item-1.jpg";
+import img2 from "../images/special-item-2.jpg";
+import goldBg from "../images/gold-bg.jpg";
 
 function FeaturedSpecial() {
   return (
-    <div id="featured-special">
+    <div className="featured-special">
       <div className="featured-special-wrap container">
         <Title
           title="Featured Specials"
@@ -13,7 +17,29 @@ function FeaturedSpecial() {
           color="#fff"
           subColor="#fff"
         />
-        <div></div>
+        <div className="special-item-wrap">
+          <FeaturedSpecialItem
+            subTitle="up to 30% off"
+            title="Kick off summer in NYC"
+            img={img1}
+          />
+          <div className="special-gold">
+            <img src={goldBg} alt="Breakfast Package" />
+            <div>
+              <h6>Only This Week</h6>
+              <h3>$5 Breakfast package at central market</h3>
+              <p>
+                Start your day with ourt $5 Breakfast on the go package at
+                central market
+              </p>
+            </div>
+          </div>
+          <FeaturedSpecialItem
+            subTitle="up to 30% off"
+            title="Kick off summer in NYC"
+            img={img2}
+          />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/FeaturedSpecialItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function FeaturedSpecialItem({ title, subTitle, img }) {
+  return (
+    <div className="special-item">
+      <img src={img} alt={title} />
+      <div>
+        <div className="black-shadow"></div>
+        <h6 className="subTitle">{subTitle}</h6>
+        <h3 className="title">{title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default FeaturedSpecialItem;
