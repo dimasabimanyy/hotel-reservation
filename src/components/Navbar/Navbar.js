82b8e87468cb1c5ce37c20b27feb6629c29a0c27
dimassabimanyy <dmsabmny@gmail.com>
2020-10-07 09:30:41 +0700

    tweak navbar sizing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,49 +14,6 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      //   <nav className="container">
-      //     <div className="logo-responsive">
-      //       <Link to="/">
-      //         <img src={logo} alt="Beach Resort" />
-      //       </Link>
-      //     </div>
-      //     <ul className="nav-links">
-      //       <li>
-      //         <Link to="/">
-      //           <h6>Home</h6>
-      //         </Link>
-      //       </li>
-      //       <li className="nav-rooms">
-      //         <Link to="/rooms">
-      //           <h6>Rooms</h6>
-      //         </Link>
-      //       </li>
-      //       <div className="logo">
-      //         <Link to="/">
-      //           <img src={logo} alt="Beach Resort" />
-      //         </Link>
-      //       </div>
-      //       <li className="nav-gallery">
-      //         <Link to="/gallery">
-      //           <h6>Gallery</h6>
-      //         </Link>
-      //       </li>
-      //       <li>
-      //         <Link to="/contact">
-      //           <h6>Contact</h6>
-      //         </Link>
-      //       </li>
-      //     </ul>
-      //     <div
-      //       onClick={this.handleToggle}
-      //       className={this.state.active ? "hamburger active" : "hamburger"}
-      //     >
-      //       <div className="burger-line"></div>
-      //       <div className="burger-line"></div>
-      //       <div className="burger-line"></div>
-      //     </div>
-      //   </nav>
-
       <nav>
         <div className="logo-responsive">
           <Link to="/">
@@ -98,12 +55,6 @@ export default class Navbar extends Component {
           <span></span>
           <span></span>
         </div>
-        {/* <button className="menu-toggle">
-          <input type="checkbox" />
-          <div></div>
-          <div></div>
-          <div></div>
-        </button> */}
       </nav>
     );
   }
