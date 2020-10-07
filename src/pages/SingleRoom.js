cd81010c1f38e8a80f089f206a61ff5115297b8a
dimassabimanyy <dmsabmny@gmail.com>
2020-10-07 14:43:10 +0700

    responsive single room

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -125,38 +125,6 @@ export default class SingleRoom extends Component {
         </section>
 
         <SingleFeaturedRooms />
-        {/* <section className="single-room">
-          <div className="single-room-images">
-            {defaultImg.map((image, index) => {
-              return <img key={index} src={image} alt={name} />;
-            })}
-          </div>
-          <div className="single-room-info">
-            <article className="desc">
-              <h3>Details</h3>
-              <p>{description}</p>
-            </article>
-            <article className="info">
-              <h3>Info</h3>
-              <h6>price : ${price}</h6>
-              <h6>Size : {size} SQFT</h6>
-              <h6>
-                Max capacity :
-                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
-              </h6>
-              <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
-              <h6>{breakfast && "free breakfast included"}</h6>
-            </article>
-          </div>
-        </section> */}
-        {/* <section className="room-extras">
-          <h6>Extras</h6>
-          <ul className="extras">
-            {extras.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </section> */}
       </section>
     );
   }
